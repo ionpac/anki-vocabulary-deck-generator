17b80b814ca51f3cd770fc80cda6f7dac6a7eb9d
Ion Paciu <ionpaciu999@gmail.com>
2022-10-14 00:16:00 +0200

    added script to list ids of deleted translation so you can remove them in anki

---
+++ list_deleted.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import json
+from update_deck import resolve_path
+import numpy as np
+
+settingsdict = json.load(open("settings.json", "r"))
+EXCEL_PATH = resolve_path(settingsdict["excel_path"])
+
+df = pd.read_excel(EXCEL_PATH)
+df = df.loc[:,["ID", "Italienisch", "Deutsch"]]
+df.replace('', np.nan, inplace=True)
+df.dropna(inplace=True)
+df["ID"] = df["ID"].map(int)
+
+# ids = df.iloc[:,["ID"]]
+ids = df["ID"].values.tolist()
+available = set(ids)
+
+lowest = 0
+highest = np.max(ids)
+all = set(range(lowest, highest + 1))
+
+deleted = sorted(all - available)
+
+print("Missing:", deleted)
