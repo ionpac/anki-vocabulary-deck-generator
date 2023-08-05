4ad79bed7926c22159e3f985e3f8c462e5b5bd96
Ion Paciu <ionpaciu999@gmail.com>
2023-08-05 18:34:33 +0200

    using google's python api for text to speech

--- update_deck.py
+++ update_deck.py
@@ -8,6 +8,7 @@ import os
 import numpy as np
 import re
 import subprocess
+from google.cloud import texttospeech
 
 def resolve_path(path : str):
     result = subprocess.run(["echo", *path.split(' ')], stdout=subprocess.PIPE, shell=True)
@@ -19,52 +20,18 @@ OUTPUT_CSV_PATH_FOR_ANKI = resolve_path(settingsdict["csv_for_anki_import"])
 AUDIO_OUTPUT_FOLDER = resolve_path(settingsdict["audio_file_output"])
 abbreviations = settingsdict["abbreviations"]
 
-def get_access_token():
-    command = "gcloud auth application-default print-access-token"
-    result = subprocess.run(command.split(), stdout=subprocess.PIPE, shell=True)
-    return result.stdout.decode("utf-8").strip()
-
-def text_to_speech(input_text : str, output_path : str, access_token : str, voice_id : int):
+def text_to_speech(text_to_speech_client, input_text : str, output_path : str, voice_id : int):
     voices = ["it-IT-Wavenet-A", "it-IT-Wavenet-B", "it-IT-Wavenet-C", "it-IT-Wavenet-D"]
     voice = voices[voice_id%len(voices)]
 
-    data = {
-        "input" : {
-            "text" : input_text
-        },
-        "voice" : {
-            "languageCode" : "it-IT",
-            "name" : voice,
-            # "ssmlGender" : "FEMALE"
-        },
-        "audioConfig" : {
-            "audioEncoding" : "MP3"
-        }
-    }
+    synthesis_input = texttospeech.SynthesisInput(text=input_text)
+    voice = texttospeech.VoiceSelectionParams(language_code="it-IT", name=voice)
+    audio_config = texttospeech.AudioConfig(audio_encoding=texttospeech.AudioEncoding.MP3)
 
-    URL = "https://texttospeech.googleapis.com/v1/text:synthesize"
+    response = text_to_speech_client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
 
-    data_json = json.dumps(data)
-
-    headers = {
-        "Authorization" : f"Bearer {access_token}",
-        "Content-Type": "application/json; charset=utf-8",
-    }
-
-    resp = requests.post(URL, data=data_json, headers=headers)
-
-    if resp.status_code == 200:
-        result_json = resp.content
-        result = json.loads(result_json)
-        audioB64 = result["audioContent"]
-        
-        bytes = base64.b64decode(audioB64)
-
-        with open(output_path, mode="wb") as fp:
-            fp.write(bytes)
-            return True
-    
-    return False
+    with open(output_path, mode="wb") as fp:
+        fp.write(response.audio_content)
 
 def prepare_for_text_to_speech(text : str):
     text = re.sub(r"\[[^\]]*\]", "", text)
@@ -108,7 +75,7 @@ def main():
     
     os.makedirs(AUDIO_OUTPUT_FOLDER, exist_ok=True)
 
-    access_token = None
+    api_client = None
     for index, row in df.iterrows():
         to_read = row["ToRead"]
         # print("ToRead", index, f"\"{to_read}\"")
@@ -122,13 +89,13 @@ def main():
             # print(f"Skipping {index} {to_read}")
             continue
 
-        if access_token is None:
-            print("Getting google cloud access token...")
-            access_token = get_access_token()
+        if api_client is None:
+            print("Creating google cloud api client...")
+            api_client = texttospeech.TextToSpeechClient()
         
         print(f"Reading {index} {to_read}")
         
-        text_to_speech(to_read, output_file, access_token, index)
+        text_to_speech(api_client, to_read, output_file, index)
 
     rows = []
     for index, row in df.iterrows():
