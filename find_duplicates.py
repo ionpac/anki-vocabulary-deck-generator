803a324317521aaef835b1a4c0c324b098f49c5f
Ion Paciu <ionpaciu999@gmail.com>
2023-10-17 21:20:39 +0200

    using stuff from config

--- find_duplicates.py
+++ find_duplicates.py
@@ -8,9 +8,12 @@ from update_deck import resolve_path
 
 settingsdict = json.load(open("settings.json", "r"))
 EXCEL_PATH = resolve_path(settingsdict["excel_path"])
+excel_header_id = settingsdict["excel_headers"]["id"]
+excel_header_native_language = settingsdict["excel_headers"]["native_language"]
+excel_header_foreign_language = settingsdict["excel_headers"]["foreign_language"]
 
 df = pd.read_excel(EXCEL_PATH)
-df = df.loc[:,["ID", "Italienisch", "Deutsch"]]
+df = df.loc[:,[excel_header_id, excel_header_foreign_language, excel_header_native_language]]
 df.replace('', np.nan, inplace=True)
 df.dropna(inplace=True)
 
@@ -43,13 +46,13 @@ def main():
 	dupFound = 0
 
 	for index, row in df.iterrows():
-		id = int(row["ID"])
-		ita = row["Italienisch"]
-		deu = row["Deutsch"]
+		id = int(row[excel_header_id])
+		foreign = row[excel_header_foreign_language]
+		native = row[excel_header_native_language]
 		
-		idToValue[id] = (ita, deu)
+		idToValue[id] = (foreign, native)
 
-		for modifier, lang in enumerate([ita, deu]):
+		for modifier, lang in enumerate([foreign, native]):
 			modified_hash = hash_meanings(lang) + modifier
 			if modified_hash in alreadyThere:
 				val = alreadyThere[modified_hash]
@@ -61,8 +64,8 @@ def main():
 		if len(ids) > 1:
 			print(f"Duplicate found: [{', '.join([str(id) for id in ids])}]")
 			for id in ids:
-				ita, deu = idToValue[id]
-				print(f"\t{ita}    {deu}")
+				foreign, native = idToValue[id]
+				print(f"\t{foreign}    {native}")
 			dupFound += 1
 
 	print(f"{dupFound} duplicates found")
